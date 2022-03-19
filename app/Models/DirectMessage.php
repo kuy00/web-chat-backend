09bd009a4f97ce4b8fdfee41be6f16140065915f
kuy <kuy472013@gmail.com>
2022-03-20 01:37:30 +0900

    채팅방 존재여부 체크 scope 추가

--- app/Models/DirectMessage.php
+++ app/Models/DirectMessage.php
@@ -16,7 +16,16 @@ class DirectMessage extends Model
 
     public function users()
     {
-        return $this->belongsToMany(User::class);
+        return $this->belongsToMany(User::class)->withPivot('created_at');
+    }
+
+    public function scopeCheckDirectMessage($query, $userId)
+    {
+        return $query->whereHas('users', function ($query) use ($userId) {
+            $query->select('direct_message_user.direct_message_id')
+                ->groupBy('direct_message_user.direct_message_id')
+                ->havingRaw("GROUP_CONCAT(direct_message_user.user_id) = '$userId'");
+        })->get();
     }
 
     public function scopeCreateDirectMesage($query, $name, $users)
